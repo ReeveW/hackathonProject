0f84588490cf9dbaa541901ccebccc708fda887b
ReeveW <reeveswilde@gmail.com>
2026-03-07 18:06:33 -0700

    made everything lowercase

--- Languages.cpp
+++ Languages.cpp
@@ -1,8 +1,8 @@
 #include "Languages.h"
 
 std::vector<LanguageData> languages = {
-    {"C++", 1985, Level::Low, Implementation::Compiled, ParentLanguage::C, MemoryManagement::Manual, "New standards are released roughly every 3 years!\n"},
-    {"Python", 1991, Level::High, Implementation::Interpreted, ParentLanguage::ABC, MemoryManagement::GarbageCollected, "Python was named after Monty Python, not the snake!\n"},
-    {"JavaScript", 1995, Level::High, Implementation::Hybrid, ParentLanguage::Java, MemoryManagement::GarbageCollected, "Did you know JavaScript was built in just 10 days?\n"},
+    {"c++", 1985, Level::Low, Implementation::Compiled, ParentLanguage::C, MemoryManagement::Manual, "New standards are released roughly every 3 years!\n"},
+    {"python", 1991, Level::High, Implementation::Interpreted, ParentLanguage::ABC, MemoryManagement::GarbageCollected, "Python was named after Monty Python, not the snake!\n"},
+    {"javascript", 1995, Level::High, Implementation::Hybrid, ParentLanguage::Java, MemoryManagement::GarbageCollected, "Did you know JavaScript was built in just 10 days?\n"},
     {}
 };
--- View.cpp
+++ View.cpp
@@ -1,9 +1,13 @@
 #include "View.h"
 #include <iostream>
+#include <cctype>
+#include <algorithm>
 
 std::string View::ask() {
     std::string input;
     std::cout << "Enter a Programming Language Guess: ";
     std::cin >> input;
+    std::transform(input.begin(), input.end(), input.begin(), 
+        [](unsigned char c){ return std::tolower(c); });
     return input;
 }
