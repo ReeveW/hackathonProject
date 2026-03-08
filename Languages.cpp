d00333db03faf1f6c275abebdadff8efbfa83c3c
Ivana Bilicic <ivana.bilicic06@gmail.com>
2026-03-07 17:47:06 -0700

    language fix

--- Languages.cpp
+++ Languages.cpp
@@ -1,6 +1,6 @@
 #include "Languages.h"
 
-std::vector<LanguageData> languages = {{"C++", 1985, Level::Low, 
-    Implementation::Compiled, ParentLanguage::C, MemoryManagement::Manual},
-{"Python", 1991, Level::High, Implementation::Interpreted, ParentLanguage::ABC,
-MemoryManagement::GarbageCollected}};
+std::vector<LanguageData> languages = {
+    {"C++", 1985, Level::Low, Implementation::Compiled, ParentLanguage::C, MemoryManagement::Manual, "New standards are released roughly every 3 years!\n"},
+    {"Python", 1991, Level::High, Implementation::Interpreted, ParentLanguage::ABC, MemoryManagement::GarbageCollected, "Python was named after Monty Python, not the snake!\n"}
+};
